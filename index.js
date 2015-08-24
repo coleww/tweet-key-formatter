b012974e9139cad39eec080468501723bcf515c5
Cole Willsea <coleww@gmail.com>
2015-08-24 05:40:17 -0700

    add fork link, handle errs better

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ var output = document.querySelector('.formatted')
 
 document.body.addEventListener('paste', function(e){
   var data = e.clipboardData.getData('text/plain')
-  var infos = data.split('\n').map(function(s){return s.replace(/\s/g, ' ')})
-  var configs = {
-    consumer_key: findIt(infos, /Consumer Key \(API Key\) /),
-    consumer_secret: findIt(infos, /Consumer Secret \(API Secret\) /),
-    access_token: findIt(infos, /Access Token /),
-    access_token_secret: findIt(infos, /Access Token Secret /)
+  try {
+    var infos = data.split('\n').map(function(s){return s.replace(/\s/g, ' ')})
+    var configs = {
+      consumer_key: findIt(infos, /Consumer Key \(API Key\) /),
+      consumer_secret: findIt(infos, /Consumer Secret \(API Secret\) /),
+      access_token: findIt(infos, /Access Token /),
+      access_token_secret: findIt(infos, /Access Token Secret /)
+    }
+    output.innerHTML = JSON.stringify(configs, null, 2);
+  } catch (e) {
+    output.innerHTML = 'WOW something broke! ' + e.name + ': ' + e.message
   }
-  output.innerHTML = JSON.stringify(configs, null, 2);
 });
 
 function findIt (strings, target) {
-  console.log(target)
-  return strings.filter(function (str) {
-    console.log(str, target, str.match(target))
-    if(str.match(target)) console.log(str, target)
+  var match = strings.filter(function (str) {
     return str.match(target)
-  })[0].replace(target, '')
+  })
+  match = match || ['']
+  return match[0].replace(target, '')
 }
